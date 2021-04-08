fea21f2feac48090ee270f7a553b4e0c1264ea60
annadrg <annaderuig@gmail.com>
2021-04-08 15:53:29 +0200

    Add dialect options to deploy to Heroku

--- config/config.js
+++ config/config.js
@@ -14,5 +14,10 @@ module.exports = {
   },
   production: {
     use_env_variable: "DATABASE_URL",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
